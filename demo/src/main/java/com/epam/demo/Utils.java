d5b3922378aa5a6bb3b9bc7f21ea80cc20bd8f89
nubik132 <mister.nubik@list.ru>
2023-12-21 20:29:19 +0300

    fix

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -6,8 +6,7 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        for (String number :
-                args) {
+        for (String number : args) {
             if (!StringUtils.isPositiveNumber(number))
                 return false;
         }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,6 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return Integer.parseInt(str) > 0;
+        return Float.parseFloat(str) > 0;
     }
 }
